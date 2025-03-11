fb725c09187ea1d3c599a157fb2d1584c996cee2
Frank-xD <sandy.demoldo@gmail.com>
2025-03-10 22:14:04 -0400

    se agrega mensaje en main

--- src/is/pkg2025/IS2025.java
+++ src/is/pkg2025/IS2025.java
@@ -14,7 +14,7 @@ public class IS2025 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HOla mundo ");
     }
     
 }
